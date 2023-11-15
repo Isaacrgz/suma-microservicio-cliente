2987069415109df8616658d50eeec71fc70504b5
Isaacrgz <isaacrgz97@gmail.com>
2023-11-14 21:56:55 -0600

    First commit to github

---
+++ src/main/java/testclientews/TestServicioSumaWS.java
@@ -0,0 +1,16 @@
+package testclientews;
+
+import clientews.servicio.ServicioSumarImplService;
+import clientews.servicio.ServicioSumarWS;
+
+public class TestServicioSumaWS {
+    public static void main(String[] args) {
+        ServicioSumarWS servicioSumar = new ServicioSumarImplService().getServicioSumarImplPort();
+        System.out.println("Ejecutando Servicio sumar WS");
+        int x = 6;
+        int y = 3;
+        int resultado = servicioSumar.sumar(x, y);
+        System.out.println("Resultado:" + resultado);
+        System.out.println("Fin Servicio Sumar WS");
+    }
+}
